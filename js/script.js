f82d4fc5ed0e5c6705e80b0048d09b38e26fd812
Facundo <faculez07@gmail.com>
2024-12-18 23:25:20 -0300

    seccioninfo

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Espera hasta que todo el contenido de la página esté cargado
-window.onload = function() {
-    // Oculta la pantalla de carga
-    document.getElementById('loadingScreen').style.display = 'none';
-    // Muestra el contenido principal
-    document.getElementById('mainContent').style.display = 'block';
-  };
-  

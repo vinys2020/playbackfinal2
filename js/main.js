9b56c7301e90f627ad0807c4850738049f208608
Facundo <faculez07@gmail.com>
2024-12-21 19:04:23 -0300

    adivinanzas

--- js/main.js
+++ js/main.js
@@ -60,22 +60,48 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll('.lazyload');
-    
-    images.forEach(img => {
-      img.addEventListener('load', () => {
-        img.classList.remove('loading');
-        img.src = img.getAttribute('data-src');  // Cambia src por data-src cuando se carga
-      });
-  
-      // Para cargar las imágenes cuando el contenido de la página esté listo
-      img.src = img.getAttribute('data-src');
-    });
-  });
-  
+
 
   
+// Objeto con las respuestas correctas para cada entrada
+const respuestasCorrectas = {
+  entrada1: "playback", // Sustituye 'respuestaCorrecta1' con la respuesta de la entrada 1
+  entrada2: "playback", // Sustituye 'respuestaCorrecta2' con la respuesta de la entrada 2
+  entrada3: "playback", // Sustituye 'respuestaCorrecta3' con la respuesta de la entrada 3
+  entrada4: "Playback"  // Sustituye 'respuestaCorrecta4' con la respuesta de la entrada 4
+};
 
+// Función para mostrar u ocultar la pista con un efecto deslizante
+function mostrarPista(entrada) {
+  var pista = document.getElementById('pista' + entrada);
   
+  // Verificar si la pista está visible
+  if (pista.style.maxHeight && pista.style.maxHeight !== '0px') {
+      // Si la pista está visible, la ocultamos con deslizamiento
+      pista.style.maxHeight = '0'; // Contrae la pista
+      setTimeout(function() {
+          pista.style.display = 'none'; // Ocultamos el bloque después de la animación
+      }, 10); // Esperamos el tiempo de la animación para ocultarlo
+  } else {
+      // Si la pista no está visible, la mostramos con deslizamiento
+      pista.style.display = 'block'; // Aseguramos que el bloque esté visible
+      setTimeout(function() {
+          pista.style.maxHeight = '1200px'; // Expande la pista
+      }, 10);
+  }
+}
+
+// Función para comprobar la respuesta
+function comprobarRespuesta(entrada) {
+  var respuestaUsuario = document.getElementById('respuesta' + entrada).value.trim();
+  var pista = document.getElementById('pista' + entrada);
+  var respuestaCorrecta = respuestasCorrectas['entrada' + entrada];
+
+  if (respuestaUsuario.toLowerCase() === respuestaCorrecta.toLowerCase()) {
+      pista.classList.add('correcto');
+      pista.classList.remove('incorrecto');
+  } else {
+      pista.classList.add('incorrecto');
+      pista.classList.remove('correcto');
+  }
+}
